c532dfac053870c7f5b547ee401dce87a440f192
Ali Soueidan <20026370+lazercaveman@users.noreply.github.com>
2025-04-09 20:56:53 +0200

    updated starter components and appearance

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,14 @@
 <template>
-  <div class="flex flex-col gap-3 p-10 fadeIn">
-    <h1 class="scss-style-option text-3xl font-bold">
-      Nuxt 3 starter
-    </h1>
-    <p>
-      {{ Store.sampleData }}
-    </p>
-    <button
-      class="px-3 py-2 w-fit bg-custom-900 text-white cursor-pointer"
-      @click="changeSampleStoreData('🥳 State has changed!')"
-    >
-      Change state
-    </button>
+  <div class="flex justify-center items-center p-10 w-screen h-screen bg-gray-900">
+    <div class="flex flex-col gap-12 items-center p-12 rounded-4xl glowUp">
+      <NuxtStarter />
+      <StarterButton class="transition-all hover:interactiveAnimation" />
+    </div>
   </div>
 </template>
 
 
 <script setup>
-  import { useSampleStore } from '~/store/sample';
-
-  const Store = useSampleStore();
-
-  function changeSampleStoreData(testString) {
-    Store.setSampleData(testString);
-  }
+  import NuxtStarter from '~/components/NuxtStarter.vue';
+  import StarterButton from '~/components/StarterButton.vue';
 </script>
-
-
-<style lang="scss" scoped>
-
-.scss-style-option:before {
-  /*
-  // Tailwind 4 should not be used with SASS, therefor SASS definitions should not use @apply:
-  // https://tailwindcss.com/docs/compatibility#sass-less-and-stylus
-  // SCSS can be use full to create complex animations (or often also instead of tailwind)
-  */
-  content: '👋 Sass @';
-}
-
-</style>
