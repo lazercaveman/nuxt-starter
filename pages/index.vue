ec21c2ccfbdb71d23e7f23367dad908059f302d7
Ali Soueidan <20026370+lazercaveman@users.noreply.github.com>
2025-04-10 15:35:46 +0200

    remove tailwind obsolete watch mode

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center items-center p-10 w-screen h-screen thousand-generated-frames-animation">
-    <div class="flex flex-col gap-12 items-center p-12 rounded-4xl drop-shadow-2xl bg-gray-900 ">
+    <div class="flex flex-col gap-12 items-center p-12 rounded-4xl drop-shadow-2xl bg-gray-900">
       <NuxtStarter />
       <StarterButton class="transition-all hover:scale-up-transition" />
     </div>
